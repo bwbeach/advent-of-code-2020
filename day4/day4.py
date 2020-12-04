499c2cb9af36490782d2a6f2cd3b16dac0b70b15
Brian Beach <coder@beachfamily.net>
2020-12-04 10:59:27 -1000

    Day 4, part 2

--- day4/day4.py
+++ day4/day4.py
@@ -1,5 +1,44 @@
 
 import itertools
+import re
+
+
+def year_range(low, high):
+    """Returns a function that tests whether year is in range, inclusive"""
+    return lambda year_str: low <= int(year_str) <= high
+
+def matches_pattern(pattern):
+    return lambda x: pattern.match(x) is not None
+
+PID_PATTERN = re.compile(r"^\d{9}$")
+HCL_PATTERN = re.compile(r"^#[0-9a-f]{6}$")
+
+def valid_height(x):
+    match = re.match(r"^(\d+)(in|cm)$", x)
+    if not match:
+        return False
+    value = int(match.group(1))
+    if match.group(2) == "in":
+        return 59 <= value <= 76
+    else:
+        return 150 <= value <= 193
+
+VALIDATORS = {
+    "byr": year_range(1920, 2002),
+    "iyr": year_range(2010, 2020),
+    "eyr": year_range(2020, 2030),
+    "hgt": valid_height,
+    "hcl": matches_pattern(HCL_PATTERN),
+    "ecl": lambda x: x in {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"},
+    "pid": matches_pattern(PID_PATTERN),
+}
+
+def is_field_valid(field_name, value):
+    """
+    Returns True iff the value is valid for the given field
+    """
+    return VALIDATORS[field_name](value)
+
 
 class Passport:
     """
@@ -18,6 +57,12 @@ class Passport:
 
     def has_all_fields(self, field_names):
         return field_names <= self.field_names
+
+    def all_fields_valid(self, field_names):
+        return all(
+            field_name in self.field_names and is_field_valid(field_name, self.data[field_name])
+            for field_name in field_names
+        )
 
     @property
     def field_names(self):
@@ -58,5 +103,13 @@ print(
         1
         for passport in parse_file(input_text)
         if passport.has_all_fields(required_fields)
+    )
+)
+print(
+    "Day 4, part 2:",
+    sum(
+        1
+        for passport in parse_file(input_text)
+        if passport.all_fields_valid(required_fields)
     )
 )
--- day4/test_day4.py
+++ day4/test_day4.py
@@ -1,7 +1,7 @@
 
-from day4.day4 import parse_file
+from day4.day4 import parse_file, is_field_valid
 
-TEST_DATA = """
+PART_1_TEST_DATA = """
 ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
 byr:1937 iyr:2017 cid:147 hgt:183cm
 
@@ -18,8 +18,34 @@ iyr:2011 ecl:brn hgt:59in
 """
 
 def test_parse():
-    passports = parse_file(TEST_DATA)
+    passports = parse_file(PART_1_TEST_DATA)
     assert 4 == len(passports)
-    assert {"ecl", "pid", "eyr", "hcl", "byr", "iyr", "cid", "ght"} == passports[0].field_names
+    assert {"ecl", "pid", "eyr", "hcl", "byr", "iyr", "cid", "hgt"} == passports[0].field_names
     assert passports[0].has_all_fields({"ecl", "pid"})
     assert not passports[0].has_all_fields({"ecl", "xxx"})
+
+VALIDATION_TEST_DATA = """
+byr valid:   2002
+byr invalid: 2003
+
+hgt valid:   60in
+hgt valid:   190cm
+hgt invalid: 190in
+hgt invalid: 190
+
+hcl valid:   #123abc
+hcl invalid: #123abz
+hcl invalid: 123abc
+
+ecl valid:   brn
+ecl invalid: wat
+
+pid valid:   000000001
+pid invalid: 0123456789
+"""
+
+def test_validate():
+    for line in VALIDATION_TEST_DATA.splitlines(keepends=False):
+        if line.strip() != "":
+            field, validity, value = line.split()
+            assert (validity == "valid:") == is_field_valid(field, value)
