641177ee920a21a22d48c4d309ed25bf829fbac4
Brian Beach <coder@beachfamily.net>
2021-01-16 16:28:26 -1000

    extend Grid to support rectanges, not just squares

--- day20-rust/src/main.rs
+++ day20-rust/src/main.rs
@@ -85,14 +85,14 @@ fn parse_tile(text: &str) -> Tile {
             .map(|&b| b)
             .collect();
 
-    Tile::new(tile_num, Grid::from_vec(tile_bytes))
+    Tile::new(tile_num, Grid::square_from_vec(tile_bytes))
 }
 
 #[test]
 fn test_parse_tile() {
     assert_eq!(
         parse_tile("Tile 1234:\n##..\n...#\n....\n..#.\n"),
-        Tile::new(1234, Grid::from_vec(b"##.....#......#.".to_vec()))
+        Tile::new(1234, Grid::square_from_vec(b"##.....#......#.".to_vec()))
     );
 }
 
@@ -155,8 +155,8 @@ impl GridPos {
 /// A square grid of things
 #[derive(Clone, Debug, Eq, PartialEq)]
 struct Grid<T> {
-    /// Both the width and the height of the grid.
-    size: usize,
+    width: usize,
+    height: usize,
 
     /// The items in the grid, in row-major order.  The first thing
     /// is the top left.  [size-1] is the top right.
@@ -166,15 +166,21 @@ struct Grid<T> {
 impl<T: Clone> Grid<T> {
     /// Creates a new grid of the given size, with every element
     /// containing the same value.
-    fn new(size: usize, initial_value: T) -> Grid<T> {
-        let items = vec![initial_value; size * size];
-        Grid { size, items }
+    fn new(width: usize, height: usize, initial_value: T) -> Grid<T> {
+        let items = vec![initial_value; width * height];
+        Grid { width, height, items }
     }
 
     /// Creates a new grid, with values supplied from a slice of values.
-    fn from_vec(items: Vec<T>) -> Grid<T> {
+    fn from_vec(width: usize, height: usize, items: Vec<T>) -> Grid<T> {
+        assert_eq!(width * height, items.len());
+        Grid { width: width, height: height, items }
+    }
+
+    /// Creates a new grid, with values supplied from a slice of values.
+    fn square_from_vec(items: Vec<T>) -> Grid<T> {
         let size = usize_sqrt(items.len());
-        Grid { size, items }
+        Grid { width: size, height: size, items }
     }
 
     /// Returns the first cell in the grid, the one at the top left.
@@ -185,9 +191,9 @@ impl<T: Clone> Grid<T> {
     /// Returns the next cell after the given one, in the order
     /// they are filled in: left-to-right, top-to-bottom.
     fn next(&self, p: GridPos) -> Option<GridPos> {
-        if p.x < self.size - 1 {
+        if p.x < self.width - 1 {
             Some(GridPos::new(p.x + 1, p.y))
-        } else if p.y < self.size - 1 {
+        } else if p.y < self.height - 1 {
             Some(GridPos::new(0, p.y + 1))
         } else {
             None
@@ -203,7 +209,7 @@ impl<T: Clone> Grid<T> {
         }
     }
 
-    /// Returns the cell above the given one.
+    /// Returns the cell to the left of the given one.
     fn left(&self, p: GridPos) -> Option<GridPos> {
         if 0 < p.x {
             Some(GridPos::new(p.x - 1, p.y))
@@ -214,46 +220,46 @@ impl<T: Clone> Grid<T> {
 
     /// Stores a value in a cell in the grid
     fn set(&mut self, p: GridPos, value: T) {
-        self.items[p.x + self.size * p.y] = value;
+        self.items[p.x + self.width * p.y] = value;
     }
 
     /// Stores a value in a cell in the grid
     fn get(&self, p: GridPos) -> &T {
-        &self.items[p.x + self.size * p.y]
+        &self.items[p.x + self.width * p.y]
     }
 
     /// Return the things at the four corners of the grid
     fn corners(&self) -> Vec<T> {
         vec![
             self.items[0].clone(),
-            self.items[self.size - 1].clone(),
-            self.items[self.size * (self.size - 1)].clone(),
-            self.items[self.size * self.size - 1].clone()
+            self.items[self.width - 1].clone(),
+            self.items[self.width * (self.height - 1)].clone(),
+            self.items[self.width * self.height - 1].clone()
         ]
     }
 
     // The top edge
     fn top_edge<'a>(&'a self) -> EdgeIterator<'a, T> {
-        EdgeIterator::new(&self.items, self.size, 0, 1)
+        EdgeIterator::new(&self.items, self.width, 0, 1)
     }
 
-    // The top edge
+    // The right edge
     fn right_edge(&self) -> Vec<T> {
-        (0..self.size).into_iter()
-            .map(|i| self.get(GridPos::new(self.size - 1, i)).clone())
+        (0..self.height).into_iter()
+            .map(|i| self.get(GridPos::new(self.width - 1, i)).clone())
             .collect()
     }
 
-    // The top edge
+    // The bottom edge
     fn bottom_edge(&self) -> Vec<T> {
-        (0..self.size).into_iter()
-            .map(|i| self.get(GridPos::new(i, self.size - 1)).clone())
+        (0..self.width).into_iter()
+            .map(|i| self.get(GridPos::new(i, self.height - 1)).clone())
             .collect()
     }
 
-    // The top edge
+    // The left edge
     fn left_edge(&self) -> Vec<T> {
-        (0..self.size).into_iter()
+        (0..self.height).into_iter()
             .map(|i| self.get(GridPos::new(0, i)).clone())
             .collect()
     }
@@ -261,23 +267,23 @@ impl<T: Clone> Grid<T> {
     /// Rotates a grid 90 degrees clockwise
     fn rotate(&self) -> Grid<T> {
         let mut elems = Vec::new();
-        for y in 0..self.size {
-            for x in 0..self.size {
-                elems.push(self.get(GridPos::new(y, self.size - x - 1)).clone());
+        for y in 0..self.width {
+            for x in 0..self.height {
+                elems.push(self.get(GridPos::new(y, self.height - x - 1)).clone());
             }
         }
-        Grid::from_vec(elems)
+        Grid::from_vec(self.height, self.width, elems)
     }
 
     /// Flips a grid on its vertial axis
     fn flip(&self) -> Grid<T> {
         let mut elems = Vec::new();
-        for y in 0..self.size {
-            for x in 0..self.size {
-                elems.push(self.get(GridPos::new(self.size - x - 1, y)).clone());
+        for y in 0..self.height {
+            for x in 0..self.width {
+                elems.push(self.get(GridPos::new(self.width - x - 1, y)).clone());
             }
         }
-        Grid::from_vec(elems)
+        Grid::from_vec(self.width, self.height, elems)
     }
 
     /// Returns grids for all rotations and reflections of this one.
@@ -295,9 +301,9 @@ impl<T: Clone> Grid<T> {
 }
 
 #[test]
-fn test_rotate_grid() {
+fn test_rotate_square() {
     let original : Grid<u8> = 
-        Grid::from_vec(
+        Grid::square_from_vec(
             vec![
                 1, 0, 0, 5, 
                 0, 2, 6, 0, 
@@ -306,7 +312,7 @@ fn test_rotate_grid() {
             ]
         );
     let rotated = 
-        Grid::from_vec(
+        Grid::square_from_vec(
             vec![
                 8, 0, 0, 1, 
                 0, 7, 2, 0, 
@@ -315,12 +321,48 @@ fn test_rotate_grid() {
             ]
         );
     let flipped =
-        Grid::from_vec(
+        Grid::square_from_vec(
             vec![
                 5, 0, 0, 1, 
                 0, 6, 2, 0, 
                 0, 3, 7, 0,
                 4, 0, 0, 8
+            ]
+        );
+    
+    assert_eq!(*original.get(GridPos::new(0, 0)), 1);
+    assert_eq!(*original.get(GridPos::new(3, 0)), 5);
+    assert_eq!(*original.get(GridPos::new(2, 1)), 6);
+    assert_eq!(original.rotate(), rotated);
+    assert_eq!(original.flip(), flipped);
+}
+
+#[test]
+fn test_rotate_rect() {
+    let original : Grid<u8> = 
+        Grid::from_vec(
+            4, 2,
+            vec![
+                1, 0, 0, 5, 
+                0, 2, 6, 0,
+            ]
+        );
+    let rotated = 
+        Grid::from_vec(
+            2, 4,
+            vec![
+                0, 1, 
+                2, 0, 
+                6, 0,
+                0, 5,
+            ]
+        );
+    let flipped =
+        Grid::from_vec(
+            4, 2,
+            vec![
+                5, 0, 0, 1, 
+                0, 6, 2, 0,
             ]
         );
     
@@ -396,7 +438,7 @@ fn part1(file_name: &str) {
 
     let choice_refs: Vec<&Tile> = choices.iter().collect();
 
-    let mut grid: Grid<Option<&Tile>> = Grid::new(size, None);
+    let mut grid: Grid<Option<&Tile>> = Grid::new(size, size, None);
     let mut used: HashSet<usize> = HashSet::new();
     let first = grid.first();
     let second = grid.next(first).unwrap();
