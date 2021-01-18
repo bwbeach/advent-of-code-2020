8e3371a8ef01cfc0cd5011bc7fed86e712771e00
Brian Beach <coder@beachfamily.net>
2021-01-18 07:24:43 -1000

    refactor: make Game struct

--- day22-rust/src/main.rs
+++ day22-rust/src/main.rs
@@ -2,10 +2,46 @@
 use std::collections::VecDeque;
 use std::fs::read_to_string;
 
+/// Cards are non-negative integers
 type Card = usize;
 
 /// The front of the queue is the top of the deck.
 type Deck = VecDeque<Card>;
+
+/// The names of the two players
+#[derive(Debug)]
+enum Player {
+    Player1,
+    Player2,
+}
+
+use crate::Player::*;
+
+/// The state of a game is the two decks
+#[derive(Debug, Eq, Hash, PartialEq)]
+struct Game(Deck, Deck);
+
+impl Game {
+    fn winner(&self) -> Player {
+        if ! self.0.is_empty() && self.1.is_empty() {
+            Player1
+        } else if self.0.is_empty() && ! self.1.is_empty() {
+            Player2
+        } else {
+            panic!("game still in progress")
+        }
+    }
+
+    fn winning_deck<'a>(&'a self) -> &'a Deck {
+        if ! self.0.is_empty() && self.1.is_empty() {
+            &self.0
+        } else if self.0.is_empty() && ! self.1.is_empty() {
+            &self.1
+        } else {
+            panic!("game still in progress")
+        }
+    }
+}
 
 /// Parses one deck, which has a header line (ignored) saying which 
 /// plaper it is, followed by one card per line, with the top of
@@ -19,17 +55,17 @@ fn parse_deck(text: &str) -> Deck {
 }
 
 /// Reads an input file containing two decks
-fn read_input(file_name: &str) -> (Deck, Deck) {
+fn read_input(file_name: &str) -> Game {
     let text = read_to_string(file_name).unwrap();
     let parts: Vec<_> = text.split("\n\n").collect();
     assert_eq!(parts.len(), 2);
-    (parse_deck(parts[0]), parse_deck(parts[1]))
+    Game(parse_deck(parts[0]), parse_deck(parts[1]))
 }
 
-/// Plays a game, returning the winning deck.
-fn play_game(init_a: &Deck, init_b: &Deck) -> Deck {
-    let mut a: Deck = init_a.clone();
-    let mut b: Deck = init_b.clone();
+/// Plays a game, returning the state when the game is done.
+fn play_game(init: &Game) -> Game {
+    let mut a: Deck = init.0.clone();
+    let mut b: Deck = init.1.clone();
 
     while ! a.is_empty() && ! b.is_empty() {
         let card_a = a.pop_front().unwrap();
@@ -44,11 +80,7 @@ fn play_game(init_a: &Deck, init_b: &Deck) -> Deck {
         }
     }
 
-    if a.is_empty() {
-        b
-    } else {
-        a
-    }
+    Game(a, b)
 }
 
 /// Returns the score to report for a winning deck.
@@ -64,11 +96,11 @@ fn score_winner(deck: &Deck) -> usize {
 
 fn main() {
     let sample = read_input("sample.txt");
-    let winning_deck = play_game(&sample.0, &sample.1);
-    println!("{:?}", winning_deck);
-    println!("{:?}", score_winner(&winning_deck));
+    let finished_sample = play_game(&sample);
+    println!("{:?}", finished_sample);
+    println!("{:?}", score_winner(finished_sample.winning_deck()));
 
     let input = read_input("input.txt");
-    println!("Part 1: {:?}", score_winner(&play_game(&input.0, &input.1)));  // 35370
+    println!("Part 1: {:?}", score_winner(play_game(&input).winning_deck()));  // 35370
 
 }
