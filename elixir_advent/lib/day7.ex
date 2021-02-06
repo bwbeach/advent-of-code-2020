a38ff33dc033fdb821ae9074cedd8c2ac5929b56
Brian Beach <coder@beachfamily.net>
2021-02-06 07:33:37 -1000

    Elixir - day 7, part2

--- elixir_advent/lib/day7.ex
+++ elixir_advent/lib/day7.ex
@@ -1,7 +1,9 @@
 defmodule Day7 do
   def run(lines) do
-    with rules = parse_rules(lines) do
-      part1(rules)
+    with rules = parse_rules(lines),
+         p1 = part1(rules),
+         p2 = bags_inside("shiny gold", rules) do
+      "part1: #{p1}   part2: #{p2}"
     end
   end
 
@@ -10,11 +12,22 @@ defmodule Day7 do
     |> Enum.count(fn {color, _} -> can_contain(color, "shiny gold", rules) end)
   end
 
-  def parse_rules(lines) do
-    for line <- lines,
-        into: %{} do
-      parse_rule(line)
+  @doc """
+  Returns the number of bags inside a bag of a given
+  color, not counting the outer bag itself.
+  """
+  def bags_inside(color, rules) do
+    # For one item in a rule, how many bags?
+    # This includes the bag inself and everything
+    # inside it.
+    bags_for_rule_item = fn {count, inner_color} ->
+      count * (1 + bags_inside(inner_color, rules))
     end
+
+    # Sum the bags for each item in the rule.
+    rules[color]
+    |> Enum.map(bags_for_rule_item)
+    |> Enum.sum()
   end
 
   def can_contain(outer_color, inner_color, rules) do
@@ -29,6 +42,14 @@ defmodule Day7 do
     end
   end
 
+  @spec parse_rules([String.t()]) :: %{String.t() => [{integer, String.t()}]}
+  def parse_rules(lines) do
+    for line <- lines,
+        into: %{} do
+      parse_rule(line)
+    end
+  end
+
   @doc """
   Parses one contents line
 
@@ -40,12 +61,14 @@ defmodule Day7 do
     iex> Day7.parse_rule("faded blue bags contain no other bags.")
     {"faded blue", []}
   """
+  @spec parse_rule(String.t()) :: {String.t(), [{integer, String.t()}]}
   def parse_rule(line) do
     with [_, lhs, rhs] = Regex.run(~r{^(.*) bags contain (.*)}, line) do
       {lhs, parse_rhs(rhs)}
     end
   end
 
+  @spec parse_rhs(String.t()) :: [{integer, String.t()}]
   def parse_rhs(rhs) do
     case Regex.run(~r{no other bag}, rhs) do
       nil ->
